0f32e2707806ad089885efc59288a93b34d2f56d
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-07-02 13:41:22 +0700

    Add HTTPS scheme enforcement for production environment in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
         Blade::directive('estimasi', function ($minutes) {
             return "<?php
         \$hours = floor($minutes / 60);
